30ffaf176e22947be984ee5b3c6e016c65919395
assafalper <alperassaf@gmail.com>
2025-07-12 19:45:15 -0700

    fix imports

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
-from backend.app import models
+from app import models
 
 def get_band_by_name(db: Session, name: str):
     return db.query(models.Band).filter(models.Band.name.ilike(name)).first()
+
 
